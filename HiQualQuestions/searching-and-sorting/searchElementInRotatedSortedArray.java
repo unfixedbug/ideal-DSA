eacf5ea302545a3c0a568f4abc04d79240c5ead7
unfixedbug <jdhiraj543@gmail.com>
2022-07-26 15:58:17 +0530

    modified binary search

---
+++ HiQualQuestions/searching-and-sorting/searchElementInRotatedSortedArray.java
@@ -0,0 +1,27 @@
+/* search element in rotated sorted array */
+public class searchElementInRotatedSortedArray {
+
+  private int search(int nums[], int target) {
+    int lo = 0, hi = nums.length - 1;
+
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (nums[mid] == target) return mid;
+
+      if (nums[lo] <= nums[mid]) {
+        if (target >= nums[lo] && target <= nums[mid]) {
+          hi = mid - 1;
+        } else {
+          lo = mid + 1;
+        }
+      } else {
+        if (target >= nums[mid] && target <= nums[hi]) {
+          lo = mid + 1;
+        } else {
+          hi = mid - 1;
+        }
+      }
+    }
+    return -1;
+  }
+}
